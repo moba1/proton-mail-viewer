4863a5c1e9fac6026e3241d0b897e22bddc395bf
moba1 <moba2@protonmail.com>
2023-08-23 23:11:48 +0900

    change node error judge method

--- src/error/index.ts
+++ src/error/index.ts
@@ -1,1 +1,6 @@
+import typeUtil from 'util/types';
+
 export { UnsupportedOsError } from './unsupported-os';
+
+export const isNodeError = (error: unknown): error is NodeJS.ErrnoException =>
+  typeUtil.isNativeError(error);
